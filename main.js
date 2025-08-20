5a9527130939a0db0033a7e4c862fa13ff4ec56c
VictorFromTheInternet <vma0430@gmail.com>
2025-08-20 12:35:47 -0500

    try catch

--- main.js
+++ main.js
@@ -3,12 +3,19 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 async function sendEmail(){
-    const mailOptions = {
-        from: process.env.SMTP_SENDER_ID,
-        to:  'vma0430@gmail.com',
-        subject: '👋 Welcome to my app!',
-        text:'Hello World!'
-    }
-    await transporter.sendMail(mailOptions)
+    try{
+        const mailOptions = {
+            from: process.env.SMTP_SENDER_ID,
+            to:  'vma0430@gmail.com',
+            subject: '👋 Welcome to my app!',
+            text:'Hello World!'
+        }
+        
+        await transporter.sendMail(mailOptions)
+        console.log('Email Sent!')
+    }catch(err){
+        console.error(err)
+    }    
+
 }
 sendEmail()        
